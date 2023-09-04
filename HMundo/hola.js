11b9e9e2ed0639f4387dc0b4dd68d1550aabbcb9
ltisera <lucastisera@gmail.com>
2023-09-04 18:55:53 -0300

    servir htmls

--- HMundo/hola.js
+++ HMundo/hola.js
@@ -1,8 +1,24 @@
 var http = require("http");
+const fs =require('fs');
+
+/// variables para despachar los html
+const paginaInicio = fs.readFileSync("cliente/index.html");
+const paginaRecetas = fs.readFileSync("cliente/recetas.html");
+
 
 var manejador = function (solicitud, respuesta){
-    console.log("Recibi peti");
-    respuesta.end("Hola mundo");
+    console.log("Ruta accedida:", solicitud.url);
+    if(solicitud.url ==='/'){
+        console.log("servi index");
+        respuesta.end(paginaInicio);
+    }else if(solicitud.url ==='/recetas'){
+        console.log("MANDAME LA RECE");
+        respuesta.end(paginaRecetas);
+    }else{
+        respuesta.writeHead(404);
+        respuesta.end("Page not found");
+    }
+
 };
 
 var servidor = http.createServer(manejador);
