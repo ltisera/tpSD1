a9bb11fb0696ff30807503fa85ffe6150359ab09
ltisera <lucastisera@gmail.com>
2023-09-05 20:40:44 -0300

    fusion server.py

--- grpc-server/server.py
+++ grpc-server/server.py
@@ -9,15 +9,16 @@ import grpc
 from concurrent import futures
 
 
+from proto import usuario_pb2 as usuario_pb2
+from proto import usuario_pb2_grpc as usuario_pb2_grpc
 from proto import receta_pb2 as receta_pb2
 from proto import receta_pb2_grpc as receta_pb2_grpc
 
+
+#### Aca te entra los request de RECETAS
 class RecetaServicer(receta_pb2_grpc.servicioRecetaServicer):
     def traerRecetasPor(self, request, context):
-        print("A ve")
         print(request.creador)
-        print("A11111")
-        
         receta1 = receta_pb2.receta(
             
             titulo ="tomates con cherry",
@@ -38,15 +39,37 @@ class RecetaServicer(receta_pb2_grpc.servicioRecetaServicer):
         respuesta = receta_pb2.traerRecetasPorResponse(recetas=listaRecetas)
         return respuesta
 
+
+
+#### Aca te entra los request de Usuario
+class UsuarioServicer(usuario_pb2_grpc.servicioUsuarioServicer):
+    def crearUsuario(self, request, context):
+        
+        print("LLEGO ALGO EHHHH")
+        print("username:" + request.username)
+        print("email:" + request.email)
+        print("password: " + request.password)
+        print("tipo: " + request.tipo)
+
+        #Logica de crear usuario conexion a la bd y eso
+        respuesta = usuario_pb2.crearUsuarioResponse(
+            username=request.username,
+            mensaje="alta exitosa"
+        )
+        return respuesta
+
 def iniciar_servidor():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     receta_pb2_grpc.add_servicioRecetaServicer_to_server(RecetaServicer(), server)
+    usuario_pb2_grpc.add_servicioUsuarioServicer_to_server(UsuarioServicer(), server)
     server.add_insecure_port('[::]:50051')  # Escucha en el puerto 50051 sin cifrado
     server.start()
+    logging.basicConfig(level=logging.DEBUG) 
+    logging.info("Servidor gRPC iniciado en el puerto 50051")
+    
     server.wait_for_termination()
     
 
 if __name__ == '__main__':
-    
     print("arrancando el server")
     iniciar_servidor()
